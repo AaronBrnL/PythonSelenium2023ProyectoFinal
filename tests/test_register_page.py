a2a044220e834d6ef5346604bd68503107a1cb76
jaaronbernal <jesus.bernal@99minutos.com>
2023-06-08 00:12:17 -0600

    adding register

---
+++ tests/test_register_page.py
@@ -0,0 +1,12 @@
+from src.page_objects.register_page import RegisterPage
+
+
+
+def test_register(web_drivers):
+    register_page = RegisterPage(*web_drivers)
+    register_page.open()
+    register_page.register("Aaron", "Bernal", "jessarn01@gmail.com", "5623613748", "qaminds12345", "qaminds12345")
+
+
+
+
